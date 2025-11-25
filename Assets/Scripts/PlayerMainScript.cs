61bc7fe4cc9d6ac83d033027aa204335055812a4
Caio Silva <caioxaviercxs@gmail.com>
2025-11-24 22:39:31 -0300

    syncing. implemented seperated scripts and created tiles for map

--- Assets/Scripts/PlayerMainScript.cs
+++ Assets/Scripts/PlayerMainScript.cs
@@ -4,36 +4,21 @@ using UnityEngine.Rendering;
 
 public class PlayerMainScript : MonoBehaviour
 {
-    private Rigidbody rb;
-    private SpriteRenderer sr;
-    private int spriteLimit;
-
+    private Camera cmr;
+    
     public float speed = 0f;
     public float minSpeed = 0.0f;
     public float maxSpeed = 9.0f;
     public float jumpSpeed = 0f;
-    public float currentSpeed = 0f;
 
-    public Camera cmr;
     public Vector3 cmrOffset = new Vector3(0.75f, 1.5f, -2.85f);
     public float minCmrX;
     public float maxCmrX;
 
-    public Sprite[] spritesIdle;
-    public Sprite[] spritesWalking;
-    public Sprite[] spritesRunning;
-    public int currentSprite = 0;
-    public float offset = 1.5f;
-    public float spriteCounter = 0.0f;
+    public bool canJump = false;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        sr = GetComponent<SpriteRenderer>();
-        
-        spriteLimit = spritesIdle.Length;
-        rb.freezeRotation = true;
-
         cmr.transform.position = transform.position + cmrOffset;
         maxCmrX = cmrOffset.x;
     }
@@ -41,17 +26,14 @@ public class PlayerMainScript : MonoBehaviour
     void Update()
     {
         cmr.transform.position = transform.position + cmrOffset;
-        spriteCounter += Time.deltaTime;
         Moviment();
+        Jump();
     }
 
     private void Moviment()
     {
         var inputHor = Input.GetAxis("Horizontal") * speed; //  a/d
         var inputVer = Input.GetAxis("Vertical") * speed;   //  w/s
-        var moviment = new Vector3(inputHor, 0, inputVer);
-
-        HandleSprites(inputHor, inputVer);
 
         if (inputHor.Equals(0) && inputVer.Equals(0))
         {
@@ -64,62 +46,23 @@ public class PlayerMainScript : MonoBehaviour
 
         if (inputHor < 0)
         {
-            sr.flipX = true;
-
             if (cmrOffset.x > minCmrX)
                 cmrOffset.x -= 0.025f;
         }
         else
         {
-            sr.flipX = false;
             if (cmrOffset.x < maxCmrX)
                 cmrOffset.x += 0.025f;
         }
-
-        Vector3 currentVelocity = rb.linearVelocity;
-        rb.linearVelocity = new Vector3(moviment.x, currentVelocity.y, moviment.z);
     }
 
-    private void HandleSprites(float hor, float ver)
+    public float GetSpeed()
     {
-        currentSpeed = rb.linearVelocity.magnitude;
-        if (spriteCounter > offset)
-        {
-            spriteCounter = 0.0f;
-
-            if (hor != 0)
-            {
-                if (ver != 0)
-                {
-                    WalkOrRunSprite();
-                }
-                else
-                {
-                    WalkOrRunSprite();
-                }
-            }
-            else
-            {
-                if (ver != 0)
-                {
-                    WalkOrRunSprite();
-                }
-                else
-                {
-                    sr.sprite = spritesIdle[currentSprite++];
-                }
-            }
-
-            if (currentSprite > spriteLimit - 1)
-                currentSprite = 0;
-        }
+        return speed;
     }
 
-    private void WalkOrRunSprite()
+    private void Jump()
     {
-        if (currentSpeed > maxSpeed / 2)
-            sr.sprite = spritesRunning[currentSprite++];
-        else
-            sr.sprite = spritesWalking[currentSprite++];
+
     }
 }
